903ce34d6deb2e9b6c5ea34d6325c9791b8d0b69
tedd mason <teddmason@gmail.com>
2021-01-14 14:06:44 +0000

    adding in units to the mview

--- database/flooddev/u_flood/3.8.0/rainfall_stations_mview.sql
+++ database/flooddev/u_flood/3.8.0/rainfall_stations_mview.sql
@@ -2,7 +2,7 @@
 
 -- DROP MATERIALIZED VIEW u_flood.rainfall_stations_mview;
 
--- select * from u_flood.rainfall_stations_mview;
+-- select * from u_flood.rainfall_stations_mview order by value_timestamp desc;
 -- REFRESH MATERIALIZED VIEW u_flood.rainfall_stations_mview WITH DATA;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS u_flood.rainfall_stations_mview
@@ -12,11 +12,12 @@ AS
 	Select s.*,
 	st_transform(st_setsrid(st_makepoint((s.easting)::double precision, (s.northing)::double precision), 27700), 4326) AS centroid, 
 	p.data_type, 
-	p.period, 
+	p.period,
+	p.units,
 	v.*
 	From u_flood.sls_telemetry_station s
 	-- get latest Rainfall parent
-	inner join (select distinct on (station, region) station, region, telemetry_value_parent_id, data_type, period
+	inner join (select distinct on (station, region) station, region, telemetry_value_parent_id, data_type, period, units
 	from u_flood.sls_telemetry_value_parent
 	where parameter = 'Rainfall'
 	order by station, region, end_timestamp desc) as p on s.station_reference = p.station and s.region = p.region
@@ -38,3 +39,4 @@ CREATE INDEX IF NOT EXISTS idx_rainfall_stations_mview_geom_gist
     ON u_flood.rainfall_stations_mview USING gist
     (centroid)
     TABLESPACE flood_indexes;
+
