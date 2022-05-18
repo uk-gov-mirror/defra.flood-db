e3ffd9f5eac72c724fce5aa8876ae1ae7f2f7a92
John Shields <shields.jf@gmail.com>
2022-05-18 14:55:54 +0100

    Revert "Target area update 2022 05 (#30)" (#31)
    This reverts commit 89e16c5db0426be4a0eee3ce2e965e7a3392821d.

--- data-load/station_ta_8km/new_generate_station_ta_8km.sql
+++
@@ -1,7 +0,0 @@
--- Note that this is a refactored version of original which originally took 4 to 6 hours depending on the size of the database
-truncate table u_flood.station_ta_8km;
-insert into u_flood.station_ta_8km
-select tc.rloi_id,a.fws_tacode from u_flood.telemetry_context_mview tc inner join u_flood.flood_alert_area a on st_dwithin(tc.geography,a.geom, 8000);
-
-insert into u_flood.station_ta_8km
-select tc.rloi_id,a.fws_tacode from u_flood.telemetry_context_mview tc inner join u_flood.flood_warning_area a on st_dwithin(tc.geography,a.geom, 8000);
