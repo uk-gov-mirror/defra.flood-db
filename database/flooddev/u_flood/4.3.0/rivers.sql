e58e7060c6ea2c10fe03cf1acd87c5879b43ca73
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2022-11-24 17:49:59 +0000

    Schema changes to support full text search

--- database/flooddev/u_flood/4.3.0/rivers.sql
+++ database/flooddev/u_flood/4.3.0/rivers.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS u_flood.river
 (
     id CHARACTER VARYING(200) COLLATE pg_catalog."default" primary key,
     name CHARACTER VARYING(200) COLLATE pg_catalog."default",
-    qualified_name CHARACTER VARYING(250) COLLATE pg_catalog."default"
+    qualified_name CHARACTER VARYING(250) COLLATE pg_catalog."default",
+    qualified_name_tokens TSVECTOR
 )
 TABLESPACE flood_tables;
 
