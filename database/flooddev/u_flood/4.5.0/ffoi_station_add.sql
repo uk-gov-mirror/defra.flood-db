71d5072ae1c0694fb63a800330003075dd8dcb33
Neil McLaughlin <neil.mclaughlin@defra.gov.uk>
2023-04-12 14:37:39 +0100

    FSR-721 Ensure script is idempotent to prevent errors on re-run (#59)

--- database/flooddev/u_flood/4.5.0/ffoi_station_add.sql
+++ database/flooddev/u_flood/4.5.0/ffoi_station_add.sql
@@ -1,5 +1,11 @@
-INSERT INTO u_flood.ffoi_station(rloi_id) VALUES (8175);
+-- clear data so script is idempotent
+DELETE FROM u_flood.ffoi_station_threshold st
+USING  u_flood.ffoi_station s
+WHERE s.ffoi_station_id = st.ffoi_station_id AND s.rloi_id = 8175;
+DELETE FROM u_flood.ffoi_station WHERE rloi_id = 8175;
 
+-- insert station and thresholds
+INSERT INTO u_flood.ffoi_station(rloi_id) VALUES (8175);
 INSERT INTO u_flood.ffoi_station_threshold (ffoi_station_id, fwis_code, value) VALUES
 ((SELECT ffoi_station_id FROM u_flood.ffoi_station WHERE rloi_id = 8175), '123FWF632', 2.2),
 ((SELECT ffoi_station_id FROM u_flood.ffoi_station WHERE rloi_id = 8175), '123FWF634', 2.2);
