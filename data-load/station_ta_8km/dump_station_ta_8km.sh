ed4aa3fac5da3e557b2c406ace1c3ff51a5641ab
LeeGordon83 <lee.gordon83@gmail.com>
2025-06-25 12:38:38 +0100

    TA update June 2025

--- data-load/station_ta_8km/dump_station_ta_8km.sh
+++ data-load/station_ta_8km/dump_station_ta_8km.sh
@@ -1,2 +1,2 @@
-/usr/lib/postgresql/13/bin/pg_dump $FLOOD_SERVICE_CONNECTION_STRING -a --inserts -t u_flood.station_ta_8km > ./station_ta_8km.sql
+pg_dump $FLOOD_SERVICE_CONNECTION_STRING -a --inserts -t u_flood.station_ta_8km > ./station_ta_8km.sql
 
